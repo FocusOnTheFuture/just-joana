e4ae45285075269b229483081251c53011f786ec
FocusOnTheFuture <focus.otfuture@gmail.com>
2024-01-25 19:48:01 -0300

    add calendar and css cardnews

--- src/Components/CardNews/index.tsx
+++ src/Components/CardNews/index.tsx
@@ -1,32 +1,37 @@
+import DateCalendarServerRequest from "../Calendar";
 import styles from "./cardnews.module.scss";
 
 const CardNews = () => {
     return (
-        <div className={styles.container}>
+        <section className={styles.section}>
+            <div className={styles.container}>
 
-            <h3>Novidades</h3>
+                <h3>Novidades</h3>
 
-            <h5 className={styles.title}>
-                Post 1
-            </h5>
-            <p className={styles.subtitle}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur imperdiet turpis in sodales.
-            </p>
+                <h5 className={styles.title}>
+                    Post 1
+                </h5>
+                <p className={styles.subtitle}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur imperdiet turpis in sodales.
+                </p>
 
-            <h5 className={styles.title}>
-                Post 2
-            </h5>
-            <p className={styles.subtitle}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur imperdiet turpis in sodales.
-            </p>
+                <h5 className={styles.title}>
+                    Post 2
+                </h5>
+                <p className={styles.subtitle}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur imperdiet turpis in sodales.
+                </p>
 
-            <h5 className={styles.title}>
-                Post 3
-            </h5>
-            <p className={styles.subtitle}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur imperdiet turpis in sodales.
-            </p>
-        </div>
+                <h5 className={styles.title}>
+                    Post 3
+                </h5>
+                <p className={styles.subtitle}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur imperdiet turpis in sodales.
+                </p>
+
+            </div>
+                <DateCalendarServerRequest/>
+        </section>
     )
 }
 
