879f4c88127194b9a7af4b8889c5682b9a5c215b
FocusOnTheFuture <focus.otfuture@gmail.com>
2024-01-26 01:56:35 -0300

    button page notFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../Components/Button";
 import styles from "./notfound.module.scss";
 import NotFoundImg from "../../assets/NotFoundImg.png";
 
 
+
 const NotFound = () => {
+
+    const navigate = useNavigate() ;
+
     return (
         <div className={styles.container}>
 
@@ -12,9 +16,12 @@ const NotFound = () => {
             <div className={styles.title}>
                 <h1>Ops! Não encontramos essa página</h1>
 
-                <Link to="/">
+                {/* <Link to="/">
                     <Button label={"Voltar para a página principal"} />
-                </Link>
+                </Link> */}
+
+                <Button label={"Voltar para a página anterior"} onClick={ () => navigate(-1) }/>
+
             </div>
         </div>
     )
