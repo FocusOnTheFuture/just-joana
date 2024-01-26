b8cbdf7bd82884bcdb3a1b4a058a07c5f8ee8797
FocusOnTheFuture <focus.otfuture@gmail.com>
2024-01-26 01:34:24 -0300

    page NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+import Button from "../../Components/Button";
+import styles from "./notfound.module.scss";
+import NotFoundImg from "../../assets/NotFoundImg.png";
 
 
 const NotFound = () => {
     return (
-        <div>
-            Error 404
+        <div className={styles.container}>
+
+            <img src={ NotFoundImg } alt="Mulher se arrumando" />
+            <div className={styles.title}>
+                <h1>Ops! Não encontramos essa página</h1>
+
+                <Link to="/">
+                    <Button label={"Voltar para a página principal"} />
+                </Link>
+            </div>
         </div>
     )
 }
